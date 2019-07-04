e4095bb68602f5ffc7b22fe56a3ffc1ecf54336f
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2019-07-04 15:16:03 +0700

    add simple author on route in server

--- scripts/server.js
+++ scripts/server.js
@@ -13,52 +13,19 @@ const { models, sequelize } = require('./../src/server/models');
 
 const secret = 'tttgalaxy';
 
+const publicAPIs = [
+  '/api/login',
+  '/api/signup'
+]
 
 async function findUserByEmail (email) {
   return await models.User.findByLogin(email)
 }
 
-app.use('/', express.static(__dirname));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // support json encoded bodies
-app.use('/api', router);
-app.use(cors());
-
-
-app.route('/api/login')
-  .get(function (req, res) {
-    res.json({
-      message: 'Please login service'
-    })
-  })
-  .post(async function (req, res) {
-    let data = req.body;
-    let user = await findUserByEmail(data.email);
-    if (user) {
-      if (user.password === data.password) {
-        let token = jwt.sign({
-          email: user.email,
-        }, secret, { expiresIn : 60*60*24 });
-
-        res.json({
-          message: 'Login success!',
-          token: token
-        })
-      } else {
-        res.json({
-          message: 'wrong username or password!'
-        })
-      }
-    } else {
-      res.json({
-        message: 'wrong username or password!'
-      })
-    }
-  })
 
 function checkAuthenticate (req, res, next) {
+  if (publicAPIs.includes(req.url)) return next();
   let token = req.body.token || req.param('token') || req.headers['x-access-token'];
-
   if (token) {
     jwt.verify(token, secret, function (err, decoded) {
       if (err) {
@@ -78,9 +45,43 @@ function checkAuthenticate (req, res, next) {
   }
 }
 
+app.use('/', express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use('/api', router);
+app.use(cors());
+app.use(checkAuthenticate);
 
-app.get('/', checkAuthenticate, function (req, res) {
-  res.send(users);
-})
+app.route('/api/login')
+  .get(function (req, res) {
+    res.json({
+      message: 'Please login service'
+    })
+  })
+  .post(async function (req, res) {
+    let data = req.body;
+    let user = await findUserByEmail(data.email);
+    if (user) {
+      if (user.password === data.password) {
+        let token = jwt.sign({
+          email: user.email,
+        }, secret, { expiresIn : 60*60*24 });
+        res.json({
+          message: 'Login success!',
+          token: token
+        })
+      } else {
+        res.json({
+          message: 'wrong username or password!'
+        })
+      }
+    } else {
+      res.json({
+        message: 'wrong username or password!'
+      })
+    }
+  })
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
