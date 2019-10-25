09ac4c1c235e295cc95ede4f204e5c1d8d6a4bd6
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2019-10-26 02:03:05 +0700

    update new portfolio

--- src/js/components/blog_v2/index.jsx
+++ src/js/components/blog_v2/index.jsx
@@ -1,47 +1,279 @@
 import React, { PureComponent } from "react";
 import styled from './styled'
 import $ from 'jquery'
+import _ from 'lodash'
+
+const avatarUrls = [
+  './images/stock-photo.jpg',
+  './images/avatar.jpg',
+  './images/avatar2.jpg',
+  './images/avatar3.jpg',
+  './images/avatar4.jpg',
+  './images/avatar5.jpg',
+  './images/avatar6.jpg',
+  './images/avatar7.jpg',
+  './images/avatar8.jpg',
+]
 
 
+
+const menuKeys = {
+  about: 'about',
+  experience: 'experience', 
+  education: 'education', 
+  skills: 'skills', 
+  projects: 'projects', 
+  the_end: 'the_end',
+}
+
+const menus = [
+  { key: menuKeys.about, text: 'ABOUT' },
+  { key: menuKeys.experience, text: 'EXPERIENCE' },
+  { key: menuKeys.education, text: 'EDUCATION' },
+  { key: menuKeys.skills, text: 'SKILLS' },
+  { key: menuKeys.projects, text: 'PROJECTS' },
+  { key: menuKeys.the_end, text: 'THE END' },
+]
+
+const experienceData = [
+  {
+    name: 'TiTan Technology',
+    time: '01/2019 to present',
+    isActive: true,
+    description: ''
+  },
+  {
+    name: 'KoLabs LLC',
+    time: '01/2019 to present',
+    description: ''
+  }
+]
+
+const educationData = [
+  {
+    name: 'TiTan Technology',
+    time: '01/2019 to present',
+    description: ''
+  }
+]
 
 class Blog extends PureComponent {
-  constructor (props) {
+  constructor(props) {
     super(props)
+    this.avatarNumber = avatarUrls.length - 1
+    this.state = {
+      avatarImageIndex: 0
+    }
+    this.menus = menus.map((menu, index) => ({ ...menu, id: `menu-item-${index}` }))
+    this.menus[0].isActive = true
+    this.experienceData = experienceData
+    this.educationData = educationData
+    this.handleClickAvatar = this.handleClickAvatar.bind(this)
+    this.renderMenu = this.renderMenu.bind(this)
+    this.renderTab = this.renderTab.bind(this)
+    this.renderTabContent = this.renderTabContent.bind(this)
+    this.renderAbout = this.renderAbout.bind(this)
+    this.renderExperience = this.renderExperience.bind(this)
+    this.renderProjects = this.renderProjects.bind(this)
+    this.renderSkills = this.renderSkills.bind(this)
+    this.renderEnd = this.renderEnd.bind(this)
   }
 
   componentDidMount() {
     $('body').css({
-        fontFamily: '"Permanent Marker", cursive',
-        margin: '18px 12vw 34px',
-        backgroundImage: 'none',
-        backgroundColor: '#B2EBF2'
+      fontFamily: '"Permanent Marker", cursive',
+      margin: '18px 12vw 34px',
+      backgroundImage: 'none',
+      backgroundColor: '#B2EBF2'
     })
-}
+  }
 
-componentWillUnmount() {
+  componentWillUnmount() {
     $('body').css({
-        background: 'url(https://res.cloudinary.com/di6vua0hr/image/upload/v1562129381/web_images/background_jath9x.png) center center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment: 'fixed'
+      background: 'url(https://res.cloudinary.com/di6vua0hr/image/upload/v1562129381/web_images/background_jath9x.png) center center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed'
     })
-}
+  }
+
+  handleClickAvatar() {
+    if (this.state.avatarImageIndex < this.avatarNumber) {
+      let newImg = this.state.avatarImageIndex + 1
+      this.setState({ avatarImageIndex: newImg })
+    } else {
+      this.setState({ avatarImageIndex: 0 })
+    }
+  }
+
+  renderMenu() {
+    return (
+      <styled.Menu>
+        {this.menus.map((menu) => (
+          <li className={menu.isActive ? 'active' : ''}
+            id={menu.id}
+            key={menu.id}
+            onClick={(e) => {
+              this.menus = this.menus.map((men) => {
+                if (men.id === e.target.id) men.isActive = true
+                else men.isActive = false
+                return men
+              })
+              this.forceUpdate()
+            }}
+            onMouseOver={(e) => {
+              this.menus = this.menus.map((men) => {
+                if (men.id === e.target.id) men.isOnHover = true
+                return men
+              })
+              this.forceUpdate()
+            }}
+            onMouseLeave={(e) => {
+              this.menus = this.menus.map((men) => {
+                if (men.id === e.target.id) men.isOnHover = false
+                return men
+              })
+              this.forceUpdate()
+            }}
+          >{menu.text}</li>
+        ))}
+      </styled.Menu>
+    )
+  }
+
+  renderTab() {
+    return this.menus.map((menu) => {
+      return (
+        <styled.Tab key={menu.id}>
+          <div className={`title ${menu.isActive ? 'active-tab' : menu.isOnHover ? 'hover-tab' : 'test'}`}>{menu.text}</div>
+        </styled.Tab>
+      )
+    })
+  }
+
+  renderAbout () {
+    return (
+      <styled.About>
+        <h1>TRẦN THANH TÀI</h1>
+        <div className='address'>643/16 · Xô Viết Nghệ Tĩnh, Bình Thạnh, TP HCM, Việt Nam </div>
+        mail: <a href='mailto:thanhtai.tttgalaxy@gmail.com'>thanhtai.tttgalaxy@gmail.com</a>
+        <p className="welcome">Welcome to visit my CV online!</p>
+        <p className="">
+          I am experienced in <strong>Javascript (ReactJS, Scala, ExpressJS), .NET Core, Java, Postgres, MS SQL Server,...etc</strong>. My
+            current target are <strong>ReactJS, .NET core</strong>.
+            Have a little experience in using <strong>AWS Console of Amazon Web Services</strong>...<br/>
+          (｡◕‿‿◕｡)
+        </p>
+        <p className="account">
+          Accounts: <br />
+          @github: <a href='https://github.com/thanhtai-personal'>https://github.com/thanhtai-personal</a><br/>
+          @npm: <a href='https://www.npmjs.com/settings/demonking/packages'>https://www.npmjs.com/demonking</a><br/>
+          @linkedIn: <a href='https://www.linkedin.com/in/tran-thanh-tai-539250129/'>https://www.linkedin.com/</a><br/>
+        </p>
+      </styled.About>
+    )
+  }
+
+  handleClickMoveDataButton (e, key, isNext) {
+    let _listData = key === 'education' ? _.clone(this.educationData) : _.clone(this.experienceData)
+    let index = _listData.findIndex((data) => data.isActive)
+    _listData[index].isActive = false
+    if (isNext) {
+      if (index > 0) index = index - 1
+    } else {
+      if (index < _listData.length - 1) index = index + 1
+    }
+    _listData[index].isActive = true
+    if (key === 'education') this.educationData = _listData
+    else this.experienceData = _listData
+    this.forceUpdate()
+  }
+
+  renderExperience (e, key, isNext) {
+    let experienceElement = ''
+    let listData = key === 'education' ? this.educationData : this.experienceData
+    listData.forEach((data) => {
+      if (data.isActive)
+        experienceElement = (<styled.Experience>
+          <div className='organization-name'>{data.name}</div>
+          <div className='organization-time'>{data.time}</div>
+          <div className='organization-des'>{data.description}</div>
+        </styled.Experience>)
+    })
+    return (
+      <React.Fragment>
+        <styled.ButtonGroup>
+          <span className='left' onClick={this.handleClickMoveDataButton.bind(this, key, true)}>{'<<<<'}</span>
+          <span className='right' onClick={this.handleClickMoveDataButton.bind(this, key, false)}>{'>>>>'}</span>
+        </styled.ButtonGroup>
+        {experienceElement}
+      </React.Fragment>
+    )
+  }
+
+  renderProjects () {
+    
+  }
+
+  renderSkills () {
+    
+  }
+
+  renderEnd () {
+    
+  }
 
 
+  renderTabContent () {
+    let contentElement = ''
+    const renderTabContentData = (data) => {
+      switch (data.key) {
+        case menuKeys.about:
+          return this.renderAbout()
+        case menuKeys.education:
+          return this.renderExperience('education')
+        case menuKeys.experience:
+          return this.renderExperience()
+        case menuKeys.projects:
+          return this.renderProjects()
+        case menuKeys.skills:
+          return this.renderSkills()
+        case menuKeys.the_end:
+          return this.renderEnd()
+      }
+    }
+    this.menus.forEach((menu) => {
+      if (menu.isActive) {
+        contentElement = (
+          <styled.TabContent key={menu.id}>
+            {renderTabContentData(menu)}
+          </styled.TabContent>
+        )
+      }
+    })
+    return contentElement
+  }
 
-  render () {
+  render() {
     return (
       <div id='blog-page'>
         <styled.SideNavWrapper>
           <styled.Title>TTTGALAXY</styled.Title>
-          <styled.ImageWrapper
-            alt="avartar"
-            title="Click to view full"
-            src="https://cv.trungquandev.com/img/trungquandev-avatar.jpg"
-          />
+          <styled.CenterWrapper>
+            <styled.ImageAvatar
+              alt="avatar"
+              title="Don't touch me!"
+              src={avatarUrls[this.state.avatarImageIndex]}
+              onClick={this.handleClickAvatar}
+            />
+          </styled.CenterWrapper>
+          <styled.CenterWrapper>
+            {this.renderMenu()}
+          </styled.CenterWrapper>
         </styled.SideNavWrapper>
         <styled.MainWrapper>
-          Main content
+          {this.renderTab()}
+          {this.renderTabContent()}
         </styled.MainWrapper>
       </div>
     )
--- src/js/components/blog_v2/styled.js
+++ src/js/components/blog_v2/styled.js
@@ -1,4 +1,61 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const color = {
+  backgroundHover: '#FFEE58',
+  textHover: '#76D7C4',
+  SideNavBackground: '#76D7C4',
+  borderImage: 'rgba(255, 255, 255, 0.4)',
+  textMenuHover: '#7D3C98',
+  activeBackgroundHover: '#F39C12',
+  textMenuActive: '#FFEE58',
+  tabBackground: '#4A235A',
+  tabActiveBackground: '#4A235A',
+  tabBorder: '#FFEE58',
+  shadowColor: '#666',
+  tabContentBackground: 'white',
+  fullName: '#2ECC71'
+}
+
+const scaleHover = keyframes`
+  0% {
+    transform: scale(0.9);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`
+
+const paddingHover = keyframes`
+  0% {
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  50% {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  100% {
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+`
+
+const tabHover = keyframes`
+  0%   {
+    padding-top: 0px;
+  }
+  50%  {
+    padding-top: 15px;
+  }
+  100% {
+    padding-top: 0px;
+  }
+`
+
+
 
 const SideNavWrapper = styled.div`
   height: 100vh; 
@@ -6,16 +63,16 @@ const SideNavWrapper = styled.div`
   min-width: 200px;
   max-width: 300px;
   position: fixed;
-  z-index: 1; 
+  z-index: 25; 
   top: 0; 
   left: 0;
-  background-color: #76D7C4;
+  background-color: ${color.SideNavBackground};
   overflow-x: hidden;
   padding-top: 20px;
 `
 
 const MainWrapper = styled.div`
-  margin-left: 20vw; 
+  padding-left: 10vw; 
   padding-top: 20px;
 `
 
@@ -23,24 +80,174 @@ const Title = styled.div`
   text-align: center; 
   margin: 0 auto;
   font-size: 26px;
+  font-family: 'chalkduster, fantasy';
   &:hover {
-    background-color: #FFEE58;
-    color: #76D7C4;
+    background-color: ${color.backgroundHover};
+    color: ${color.textHover};
   }
 `
 
-const ImageWrapper = styled.image`
-  max-width: 11rem;
-  max-height: 11rem;
-  border: 0.5rem solid rgba(255, 255, 255, 0.4);
+const CenterWrapper = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 25px;
+`
+
+const ImageAvatar = styled.img`
+  max-width: 20rem;
+  max-height: 20rem;
+  border: 0.5rem solid ${color.borderImage};
   vertical-align: middle;
   width: 200px;
-  height: 100px;
+  height: 200px;
+  margin: 0 auto;
+  border-radius: 50%;
+  text-align: center;
+  cursor: pointer;
+  &:hover {
+    animation: ${scaleHover} 0.5s linear infinite;
+  }
+`
+
+const Menu = styled.ul`
+  width: 90%;
+  margin-right: 5%;
+  li {
+    cursor: pointer;
+    list-style-type: none;
+    margin-top: 5px;
+    line-height: 200%;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    &:hover {
+      background-color: ${color.backgroundHover};
+      color: ${color.textMenuHover};
+      animation: ${paddingHover} 0.5s linear;
+      padding-top: 15px;
+      padding-bottom: 15px;
+    }
+    &:active {
+      background-color: ${color.activeBackgroundHover};
+      color: ${color.textMenuActive};
+      padding-top: 15px;
+      padding-bottom: 15px;
+    }
+    &.active {
+      background-color: ${color.activeBackgroundHover};
+      color: ${color.textMenuHover};
+      padding-top: 10px;
+      padding-bottom: 10px;
+    }
+  };
+`
+
+const Tab = styled.div`
+  opacity: 0.7;
+  width: 100%;
+  height: auto;
+  background-color: ${color.tabBackground}
+  .title {
+    padding: 8px 16px;
+    float: left;
+    width: auto;
+    border: none;
+    display: block;
+    outline: 0;
+    cursor: pointer;
+    border-bottom: solid 2px ${color.tabBorder};
+    border-left: solid 1px ${color.tabBorder};
+    border-right: solid 1px ${color.tabBorder};
+    box-shadow: 10px 10px 10px ${color.shadowColor};
+  };
+  .hover-tab {
+    animation: ${tabHover} 0.5s linear infinite;
+  };
+  .active-tab {
+    background-color: ${color.tabActiveBackground}
+    color: ${color.backgroundHover}
+    box-shadow: 20px 20px 20px ${color.shadowColor};
+  }
+`
+
+const TabContent = styled.div`
+  width: 100%;
+  height: 100%;
+  float: left;
+  padding-top: 4rem;
+  padding-bottom: 2rem;
+  padding-left: 4rem;
+  padding-right: 1rem;
+  min-width: 60vw;
+  max-width: 100vw;
+  min-height: 85vh;
+  max-height: 100vh;
+  background-color: ${color.tabContentBackground};
+  opacity: 0.7;
+  box-shadow: 20px 20px 20px ${color.shadowColor};
+  over-flow: scroll;
+`
+
+const About = styled.div`
+  h1 {
+    font-size: 3rem;
+    line-height: 2.5rem;
+    margin-bottom: 0;
+    text-transform: uppercase;
+    color: ${color.fullName};
+    font-family: Saira Extra Condensed, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  }
+  .welcome {
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+  .account {
+    padding-top: 2rem;
+  }
+  strong {
+    font-weight: bold;
+  }
+`
+const Experience = styled.div`
+  .organization-name {
+    text-transform: uppercase;
+    font-size: 2rem;
+    color: ${color.fullName};
+    float: left;
+    padding-top: 3rem;
+  }
+  .organization-time {
+    float: right;
+    color: ${color.textMenuHover}
+    text-decoration-line: overline underline;
+    padding-top: 3rem;
+  }
+  .organization-des {
+    padding-top: 1rem;
+  }
+`
+
+const ButtonGroup = styled.div`
+  cursor: pointer;
+  .left {
+    float: left
+    padding-right: 2rem;
+  }
+  .right {
+    float: right;
+    padding-left: 2rem;
+  }
 `
 
 export default {
   SideNavWrapper,
   MainWrapper,
   Title,
-  ImageWrapper
+  CenterWrapper,
+  ImageAvatar,
+  Menu,
+  Tab,
+  TabContent,
+  About,
+  Experience,
+  ButtonGroup
 }
