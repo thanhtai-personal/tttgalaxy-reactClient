93e79b3cb53f3a580905228a8351c18eb6569857
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2019-10-26 11:27:55 +0700

    detect mobi

--- src/js/containers/blog/blog.container.jsx
+++ src/js/containers/blog/blog.container.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-// import Blog from '../../components/blog/blog'
+import BlogMobi from '../../components/blog/blog'
 import Blog from '../../components/blog_v2'
 
 
 class BlogContainer extends Component {
   render() {
-    return (
-      <Blog
-      />
-    )
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) 
+    ? (<BlogMobi />) : (<Blog />)
   }
 }
 
