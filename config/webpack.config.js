43cee040a8347ecce06c6b48a1d00df1ef6495fb
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2019-10-30 01:29:27 +0700

    webpack  audio

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -324,7 +324,7 @@ module.exports = function(webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.mp3$/, /\.mp4$/],
               loader: require.resolve('url-loader'),
               options: {
                 limit: 10000,
