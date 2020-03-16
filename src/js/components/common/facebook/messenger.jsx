b64c4e09e31ded3dea8a3430eddf0f16af229664
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2020-03-16 10:12:56 +0700

    move facebook messenger js to component

--- src/js/components/common/facebook/messenger.jsx
+++ src/js/components/common/facebook/messenger.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import { useEffect } from 'react'
 
 const FacebookMessenger = () => {
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: 'v6.0'
+      })
+    }
+  
+    let js, fjs = document.getElementsByTagName('script')[0]
+    if (document.getElementById('facebook-jssdk')) return
+    js = document.createElement('script')
+    js.id = 'facebook-jssdk'
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+    fjs.parentNode.insertBefore(js, fjs)
+  }, [])
+
   return (
     <div className="fb-customerchat" attribution='setup_tool' pageId='104839157812804' themeColor='#20cef5'></div>
   )
