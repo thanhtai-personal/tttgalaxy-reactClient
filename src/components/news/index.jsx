1d1158db77108b6e26d1f24731d678ae707d9990
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2020-03-17 23:28:47 +0700

    action menu init

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ActionMenu from './actionMenu'
 import { NewsStyled } from './styled.js'
 
 const NewsComponent = (props) => {
+  const [dataKey, setDataKey] = useState('24h')
+  const { news = [] } = props
+  const data = news.find(n => n.key === dataKey) || { title: '24h', src: 'https://www.24h.com.vn/' }
   return (
     <NewsStyled>
-
+      <ActionMenu
+        setData={setDataKey}
+      />
+      <iframe title={data.title} src={data.src}>
+      </iframe>
     </NewsStyled>
   )
 }
--- src/components/news/styled.js
+++ src/components/news/styled.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
 export const NewsStyled = styled.div`
-  
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
 `
